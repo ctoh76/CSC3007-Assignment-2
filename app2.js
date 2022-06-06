b25742347b772d00acd7187e810991af75951e30
Clarence Toh-1901842 <2508447T@student.gla.ac.uk>
2022-06-06 23:02:41 +0800

    3rd push

--- app2.js
+++ app2.js
@@ -1,10 +1,1 @@
-console.log(d3);
-
-// d3.select('h1').style('color', 'red')
-// .attr('class', 'heading')
-// .text('Updated h1 tag');
-
-// d3.select('body').append('p').text('1st Para');
-// d3.select('body').append('p').text('2nd Para');
-// d3.select('body').append('p').text('3rd Para');
-
+console.log(dataset);
